25b08ae98f5a2d2abc7d0a964be1a20db7ef8f59
pranavlabdhe <plabdhe2000@gmail.com>
2024-02-15 20:32:32 +0530

    Added favicon

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,3 +1,30 @@
+// import React from "react";
+// import MovieCard from "./MovieCard";
+// import { Scrollbars } from "react-custom-scrollbars";
+
+// const MovieList = ({ title, movies }) => {
+//   console.log("ML", movies);
+//   return (
+//     <div className="px-6">
+//       <h1 className="fw-b text-capitalize py-4">{title}</h1>
+//       <div className="flex overflow-x-scroll pt-3 scroll-container">
+//         <div className="flex">
+//           {movies?.map((m) => {
+//             return (
+//             <div key={m.id}>
+//              <MovieCard  poster={m.poster_path} desc={m.overview} release={m.release_date} movie_title={m.title}/>
+//             </div>
+//            );
+//           })}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default MovieList;
+
+
 import React from "react";
 import MovieCard from "./MovieCard";
 import { Scrollbars } from "react-custom-scrollbars";
@@ -12,7 +39,13 @@ const MovieList = ({ title, movies }) => {
           {movies?.map((m) => {
             return (
             <div key={m.id}>
-             <MovieCard  poster={m.poster_path} desc={m.overview} release={m.release_date} movie_title={m.title}/>
+             <MovieCard
+               poster={m.poster_path}
+               desc={m.overview}
+               release={m.release_date}
+               movie_title={m.title}
+               trailerVideoKey={m.trailerVideoKey} // Pass the trailer video key
+             />
             </div>
            );
           })}
